acfe9fd7cafe6c007b2b0d3db9357950c851eae6
Théo METOIS <theometois05@gmail.com>
2024-11-08 18:05:19 +0100

    ajout fct contain add_mot et toString dans la classe Joueur

--- project boggle/Joueur.cs
+++ project boggle/Joueur.cs
@@ -8,5 +8,42 @@ namespace project_boggle
 {
     internal class Joueur
     {
+        private string nom;
+        private int score;
+        private List<string> liste;
+
+        public Joueur (string nom)
+        {
+            this.nom = nom;
+        }
+        public bool Contain(string mot)
+        {
+            bool valeur = false;
+            for(int i = 0; i < this.liste.Count; i++)
+            {
+                if(liste[i] == mot)
+                {
+                    valeur = true;
+                }
+            }
+            return valeur;
+        }
+        public void Add_Mot(string mot)
+        {
+            if(!Contain(mot))
+            {
+                this.liste.Add(mot);
+            }
+        }
+
+        public string toString()
+        {
+            string phrase = this.nom+" a actuellement "+this.score+" points avec les mots ";
+            for (int i = 0;i < this.liste.Count; i++)
+            {
+                phrase += this.liste[i]+" ";
+            }
+            return phrase;
+        }
     }
 }
