0119ac4d4e68af5085a5ec3fa29ff1a6c539b6c2
theom <theom@C-LE-PC-DE-T-O>
2024-11-08 16:51:14 +0100

    Ajoutez des fichiers projet.

---
+++ project boggle/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace project_boggle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
